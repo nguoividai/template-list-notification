a814ba91d5f893ea606e76aebdd0f1d0cbd42e40
nguoividai <nguoividai@outlook.com.vn>
2022-10-20 15:30:16 +0700

    initial template

---
+++ script/app.js
@@ -0,0 +1,123 @@
+(function () {
+  "use strict";
+  window.onload = function () {
+    console.log("ready!");
+    let notifications = [].concat(window.notifications);
+
+    const handlerMarkAllAsRead = () => {
+      notifications = notifications.map((e) => ({ ...e, isNew: false }));
+    };
+
+    const renderList = () => {
+      const listRenderId = document.getElementById("top-list-binding");
+      for (let i in notifications) {
+        listRenderId.innerHTML += `
+                <div class="top-list-notification" data-id="${
+                  notifications[i]?.id
+                }">
+                    <div class="top-notification-avatar-area">
+                        <img
+                        class="top-notification-avatar"
+                        src="${notifications[i]?.avatarSrc}"
+                        alt="avatar"
+                        />
+                    </div>
+                    <div class="top-notification-content-area">
+                        <div class="top-notification-title">
+                        ${notifications[i]?.title}
+
+                        ${
+                          notifications[i]?.isNew
+                            ? '<span class="top-badge-dot"></span>'
+                            : ""
+                        }
+                        
+                        </div>
+                        <div class="top-notification-extra">
+                        ${notifications[i]?.time}</div>
+
+                        ${
+                          notifications[i]?.description?.length > 0
+                            ? ` <div class="top-notification-description">
+                            ${notifications[i]?.description}
+                            </div>`
+                            : ""
+                        }
+                    </div>
+                    ${
+                      notifications[i]?.pictureSrc
+                        ? `
+                        <div class="top-notification-picture-area">
+                                <img src="./assets/images/image-chess.webp" alt="picture" />
+                        </div>`
+                        : ""
+                    }
+                   
+                </div>
+        `;
+      }
+    };
+
+    const removeList = () => {
+      const listRenderId = document.getElementById("top-list-binding");
+      listRenderId.innerHTML = "";
+    };
+
+    const getTotalUnread = () => {
+      document.getElementById("total-unread").innerText =
+        notifications?.filter((e) => e.isNew)?.length || 0;
+    };
+
+    const refreshList = () => {
+      removeList();
+      renderList();
+      getTotalUnread();
+    };
+
+    const addNewNotification = () => {
+      const newItem = {
+        id: +notifications?.[0].id + 1 || 1,
+        avatarSrc: "../../assets/images/avatar-angela-gray.webp",
+        title: "<a>New notification</a>  has been created",
+        isNew: true,
+        description: "",
+        time: "Now",
+      };
+      notifications.unshift(newItem);
+    };
+
+    document.getElementById("button-mark-all-as-read").onclick = () => {
+      handlerMarkAllAsRead();
+      refreshList();
+    };
+
+    document.getElementById("button-add-new").onclick = () => {
+      addNewNotification();
+      refreshList();
+    };
+
+    // document.getElementById("button-100-add-new").onclick = () => {
+    //   for (let i = 0; i < 100; i++) {
+    //     addNewNotification();
+    //   }
+    //   refreshList();
+    // };
+
+    document.getElementById("top-list-binding").onclick = (event) => {
+      const elementNotification = event?.path?.[2];
+      if (elementNotification) {
+        const dataId = elementNotification.dataset?.id;
+        notifications = notifications.map((e) =>
+          e?.id === +dataId ? { ...e, isNew: false } : { ...e }
+        );
+        refreshList();
+      }
+    };
+
+    const initial = () => {
+      renderList();
+    };
+
+    initial();
+  };
+})();
---
+++ script/constants/seeder.js
@@ -0,0 +1,49 @@
+var notifications = [
+  {
+    id: 1,
+    avatarSrc: "../../assets/images/avatar-angela-gray.webp",
+    title:
+      "<a>Angela</a> react to your recent post <a>My First tournament today</a>",
+    isNew: true,
+    description: "",
+    time: "1m ago",
+  },
+  {
+    id: 2,
+    avatarSrc: "../../assets/images/avatar-anna-kim.webp",
+    title: "<a>Anna</a> followed you ",
+    isNew: true,
+    time: "10m ago",
+  },
+  {
+    id: 3,
+    avatarSrc: "../../assets/images/avatar-jacob-thompson.webp",
+    title: "<a>Thompson</a> has joined your group",
+    isNew: true,
+    time: "1 day ago",
+  },
+  {
+    id: 4,
+    avatarSrc: "../../assets/images/avatar-kimberly-smith.webp",
+    title: "<a>Kimberly</a> sent you a private message",
+    isNew: false,
+    time: "1 day ago",
+    description: "Hello, thanks for setting up the Chess Club.",
+  },
+  {
+    id: 5,
+    avatarSrc: "../../assets/images/avatar-rizky-hasanuddin.webp",
+    title: "<a>Hasanuddin</a> comment on your picture",
+    isNew: false,
+    time: "1 day ago",
+    pictureSrc: "/assets/images/image-chess.webp",
+  },
+  {
+    id: 6,
+    avatarSrc: "../../assets/images/avatar-nathan-peterson.webp",
+    title:
+      "<a>Nathan</a> reacted to your recent post <a> 5 end-game strategies to increase your win rate </a> and comment on this post ",
+    isNew: false,
+    time: "3 weeks ago",
+  },
+];
